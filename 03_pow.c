e041b5d8cb3b70dd0b7b594d8b504c90bbe29936
borodulina <alinaborodulina12009@gmail.com>
2021-09-27 23:18:18 +0700

    add new file

---
+++ 03_pow.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    double x, y, power;
+    if(scanf("%lf %lf", &x, &y) == 2){
+        power = pow(x, y);
+        printf("%lf\n", power);
+    }
+
+    return 0;
+}
